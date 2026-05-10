d5388a202f08e2f0cf8d879b517b364d7be00b7f
Hemant Singh Rawat <112156298+Hemantsrawat15@users.noreply.github.com>
2026-05-10 18:26:12 +0530

    Time: 1 ms (100.00%) | Memory: 50.9 MB (54.32%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int first = func(nums,goal);
+        int second = func(nums,goal-1);
+        return first - second;
+    }
+    public static int func(int[] nums, int goal){
+        if(goal<0) return 0;
+        int n = nums.length;
+        int left = 0, right = 0,sum = 0, count = 0;
+        while(right<n){
+            sum += nums[right];
+            while(sum>goal){
+                sum -= nums[left];
+                left++;
+            }
+            count+= right-left+1;
+            right++;
+        }
+        return count;
+    }
+}
